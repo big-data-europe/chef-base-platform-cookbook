2b07aa9dfcbae44081406db7bcdc5927c752e020
Ivan Ermilov <earthquakesan@gmail.com>
2015-12-07 15:25:34 +0100

    condition for not formatting hdfs path if exists already

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,3 @@
 default['java']['jdk_version'] = '7'
 default['hadoop']['core_site']['fs.defaultFS'] = 'hdfs://node102.local'
+default['hadoop']['hdfs_site']['dfs.name.dir'] = 'file:///tmp/hadoop-hdfs/dfs/name'
