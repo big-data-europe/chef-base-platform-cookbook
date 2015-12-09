21df70d13f159dfa6f9de95597b95b4524408c6e
Ivan Ermilov <earthquakesan@gmail.com>
2015-12-10 00:21:08 +0100

    attributes moved to the recipes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,1 @@
 default['java']['jdk_version'] = '7'
-default['hadoop']['core_site']['fs.defaultFS'] = 'hdfs://node102.local'
-default['hadoop']['hdfs_site']['dfs.name.dir'] = 'file:///tmp/hadoop-hdfs/dfs/name'
