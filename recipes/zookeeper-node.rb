ca615ab0fbe4efeae47af77596d28c2986b3ff17
Ivan Ermilov <earthquakesan@gmail.com>
2015-12-10 00:19:56 +0100

    rename development item from databag to production

--- recipes/zookeeper-node.rb
+++ recipes/zookeeper-node.rb
@@ -8,7 +8,6 @@
 #
 
 data_bag_zookeeper = data_bag_item('config', 'zookeeper')
-node.default['zookeeper-cluster']['config']['instance_name'] = node['ipaddress']
-node.default['zookeeper-cluster']['config']['ensemble'] = data_bag_zookeeper['development']
 node.default['zookeeper-cluster']['config']['instance_name'] = node["fqdn"]
+node.default['zookeeper-cluster']['config']['ensemble'] = data_bag_zookeeper['production']
 include_recipe 'zookeeper-cluster::default'
